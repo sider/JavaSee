426589f16f43f6a39db1a5185fe838f03e4e073b
Kota Mizushima <mizukota@gmail.com>
2019-05-30 21:39:38 +0900

    Remove unneeded comments

--- src/main/java/com/github/kmizu/java_see/Main.java
+++ src/main/java/com/github/kmizu/java_see/Main.java
@@ -224,19 +224,4 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
-
-    /*
-    def self.source_root
-      File.join(__dir__, "../..")
-    end
-
-    include Thor::Actions
-
-    desc "init", "Generate Querly config file (querly.yml)"
-    def init()
-      copy_file("template.yml", "querly.yml")
-    end
-
-end
-            */
 }
