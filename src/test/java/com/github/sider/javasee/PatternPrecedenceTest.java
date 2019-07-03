df1529a160717f58883b06618e360e8d8888e205
Kota Mizushima <mizukota@gmail.com>
2019-07-03 20:36:19 +0900

    Add tests about operator precedence
    Let `P(op)` be precedence of `op`:
    - test `P(<<) == P(>>) && P(>>) == P(>>>)`
      - `P(<<) == P(>>) == P(>>>)`
    - test `P(*) > P(>>)`

--- src/test/java/com/github/sider/javasee/PatternPrecedenceTest.java
+++ src/test/java/com/github/sider/javasee/PatternPrecedenceTest.java
@@ -17,6 +17,97 @@ public class PatternPrecedenceTest {
         return new JavaSeeParser(new StringReader(input));
     }
 
+
+    @Test
+    @DisplayName("Check <<'s precedence  == >>'s precedence")
+    public void testLeftShiftAndRightShift() throws Exception {
+        AST.Expression e1;
+        AST.BinaryExpression e2;
+
+        // 1 << 2 >> 3 == (1 << 2) >>  3
+        e1 = parser("1 << 2 >> 3").WholeExpression();
+        assertTrue(e1 instanceof AST.RightShiftExpression);
+        e2 = (AST.BinaryExpression) e1;
+        assertTrue(e2.lhs instanceof AST.LeftShiftExpression);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+        assertEquals(3, ((AST.IntLiteral)e2.rhs).value);
+
+        //e2.lhs must be (1 << 2);
+        e2 = (AST.BinaryExpression) e2.lhs;
+        assertTrue(e2.lhs instanceof AST.IntLiteral);
+        assertEquals(1, ((AST.IntLiteral)e2.lhs).value);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+        assertEquals(2, ((AST.IntLiteral)e2.rhs).value);
+
+        // 1 >> 2 << 3 == (1 >> 2) << 3
+        e1 = parser("1 >> 2 << 3").WholeExpression();
+        assertTrue(e1 instanceof AST.LeftShiftExpression);
+        e2 = (AST.BinaryExpression) e1;
+        assertTrue(e2.lhs instanceof AST.RightShiftExpression);
+        assertTrue(e2.rhs instanceof AST.IntLiteral); assertEquals(3, ((AST.IntLiteral)e2.rhs).value);
+
+        //e2.lhs must be (1 >> 2);
+        e2 = (AST.BinaryExpression) e2.lhs;
+        assertTrue(e2.lhs instanceof AST.IntLiteral);
+        assertEquals(1, ((AST.IntLiteral)e2.lhs).value);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+        assertEquals(2, ((AST.IntLiteral)e2.rhs).value);
+    }
+
+    @Test
+    @DisplayName("Check >>'s precedence  == >>>'s precedence")
+    public void testRightShiftAndUnsignedRightShift() throws Exception {
+        AST.Expression e1;
+        AST.BinaryExpression e2;
+
+        // 1 >> 2 >>> 3 == (1 >> 2) >>> 3
+        e1 = parser("1 >> 2 >>> 3").WholeExpression();
+        assertTrue(e1 instanceof AST.UnsignedRightShiftExpression);
+        e2 = (AST.BinaryExpression) e1;
+        assertTrue(e2.lhs instanceof AST.RightShiftExpression);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+        assertEquals(3, ((AST.IntLiteral)e2.rhs).value);
+
+        //e2.lhs must be (1 >> 2);
+        e2 = (AST.BinaryExpression) e2.lhs;
+        assertTrue(e2.lhs instanceof AST.IntLiteral);
+        assertEquals(1, ((AST.IntLiteral)e2.lhs).value);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+        assertEquals(2, ((AST.IntLiteral)e2.rhs).value);
+
+        // 1 >>> 2 >> 3 == (1 >>> 2) >> 3
+        e1 = parser("1 >>> 2 >> 3").WholeExpression();
+        assertTrue(e1 instanceof AST.RightShiftExpression);
+        e2 = (AST.BinaryExpression) e1;
+        assertTrue(e2.lhs instanceof AST.UnsignedRightShiftExpression);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+
+        //e2.lhs must be (1 >>> 2);
+        e2 = (AST.BinaryExpression) e2.lhs;
+        assertTrue(e2.lhs instanceof AST.IntLiteral);
+        assertEquals(1, ((AST.IntLiteral)e2.lhs).value);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+        assertEquals(2, ((AST.IntLiteral)e2.rhs).value);
+    }
+
+
+    @Test
+    @DisplayName("Check * has higer precedence than >>")
+    public void testMultiplicationAndRightShift() throws Exception {
+        AST.Expression e1;
+        AST.BinaryExpression e2;
+        e1 = parser("1 >> 2 * 3").WholeExpression();
+        assertTrue(e1 instanceof AST.RightShiftExpression);
+        e2 = (AST.BinaryExpression) e1;
+        assertTrue(e2.lhs instanceof AST.IntLiteral);
+        assertTrue(e2.rhs instanceof AST.Multiplication);
+
+        e2 = (AST.BinaryExpression) e2.rhs;
+        assertTrue(e2.lhs instanceof AST.IntLiteral);
+        assertTrue(e2.rhs instanceof AST.IntLiteral);
+    }
+
+
     @Test
     @DisplayName("Check +'s precedence  == -'s precedence")
     public void testAdditionAndSubtraction() throws Exception {
@@ -43,8 +134,7 @@ public class PatternPrecedenceTest {
         assertTrue(e1 instanceof AST.Addition);
         e2 = (AST.BinaryExpression) e1;
         assertTrue(e2.lhs instanceof AST.Subtraction);
-        assertTrue(e2.rhs instanceof AST.IntLiteral);
-        assertEquals(3, ((AST.IntLiteral)e2.rhs).value);
+        assertTrue(e2.rhs instanceof AST.IntLiteral); assertEquals(3, ((AST.IntLiteral)e2.rhs).value);
 
         //e2.lhs must be (1 - 2);
         e2 = (AST.BinaryExpression) e2.lhs;
@@ -89,6 +179,7 @@ public class PatternPrecedenceTest {
         assertTrue(e2.rhs instanceof AST.IntLiteral);
         assertEquals(2, ((AST.IntLiteral)e2.rhs).value);
     }
+
 
     @Test
     @DisplayName("Check * has higer precedence than +")
