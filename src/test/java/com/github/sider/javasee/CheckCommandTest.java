f9c79ec3a814c642de688af5958e842302d5dda5
Kota Mizushima <mizukota@gmail.com>
2019-07-10 22:56:41 +0900

    Add a simple test for check command

---
+++ src/test/java/com/github/sider/javasee/CheckCommandTest.java
@@ -0,0 +1,36 @@
+package com.github.sider.javasee;
+
+import com.github.sider.javasee.command.CheckCommand;
+import com.github.sider.javasee.command.TestCommand;
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CheckCommandTest {
+    @Test
+    public void testCheckPrintln() throws Exception {
+        var yaml = "rules:\n" +
+                   "  - id: check-println\n" +
+                   "    pattern: _.println(...)\n" +
+                   "    message: |\n" +
+                   "      println() is detected\n";
+        var configFile = File.createTempFile("abc", "def");
+        Files.writeString(configFile.toPath(), yaml);
+        CheckCommand check = new CheckCommand();
+        check.optionConfig = configFile.getPath();
+        check.paths = List.of("src/test/resources/check");
+        var out = new ByteArrayOutputStream();
+        var status = check.start(new PrintStream(out), System.err);
+        assertEquals(JavaSee.ExitStatus.FAILURE, status);
+        assertEquals(
+           "src/test/resources/check/Println.java:3:9\t\u001B[31m        System.out.println(\"test check command\");\u001B[0m\tprintln() is detected(check-println)\n",
+            new String(out.toByteArray()))
+        ;
+    }
+}
