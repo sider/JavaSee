4c0daf3c99c31cc6fc1577cd464d536bce5d6ba0
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-09-15 01:41:39 +0900

    Reconsider distribution
    - Refactor `build.gradle`.
    - Add `settings.gradle` to change the project name: `JavaSee` -> `javasee`
    - Replace `scripts/release.sh` with `./gradlew build`.
    - Refactor `Dockerfile`.
    - Get the version number from `Implementation-Version` in the JAR manifest.

--- scripts/javasee.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@rem Assumes the following directory structure.
-@rem   javasee/     => The top dir
-@rem     lib/       => .jar file
-@rem     bin/       => executable files
-
-set TOPDIR=%~dp0..
-java -Djavasee.name=javasee.bat -jar "%TOPDIR%\lib\JavaSee-all.jar" %*
